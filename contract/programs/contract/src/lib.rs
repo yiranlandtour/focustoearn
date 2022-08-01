04a9d89c24bab6d6b04730e8951d4a32c1e44caf
yiranlandtour <yiranlandtour@163.com>
2022-08-01 20:27:35 +0800

    bug fix

--- contract/programs/contract/src/lib.rs
+++ contract/programs/contract/src/lib.rs
@@ -4,7 +4,7 @@ use anchor_spl::token;
 use anchor_spl::token::{MintTo, Token};
 use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v2};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8LDy3uUATrq46xPaz4dc5hLJeNTRuuXUd7Fqmy5Y8CJ2");
 
 #[program]
 pub mod contract {
@@ -14,6 +14,111 @@ pub mod contract {
         Ok(())
     }
 
+    pub fn init_token(ctx: Context<Initialize>) -> Result<()>{
+        Ok(())
+    }
+
+    pub fn nft_transfer(ctx: Context<Initialize>) -> Result<()>{
+        Ok(())
+    }
+
+    pub fn withdraw_token(ctx: Context<Initialize>) -> Result<()>{
+        Ok(())
+    }
+    
+    pub fn mint_nft(
+        ctx: Context<MintNFT>,
+        creator_key: Pubkey,
+        uri: String,
+        title: String,
+    ) -> Result<()> {
+        msg!("Initializing Mint NFT");
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+    msg!("CPI Accounts Assigned");
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        msg!("CPI Program Assigned");
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        msg!("CPI Context Assigned");
+        token::mint_to(cpi_ctx, 1)?;
+        msg!("Token Minted !!!");
+        let account_info = vec![
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        msg!("Account Info Assigned");
+        let creator = vec![
+            mpl_token_metadata::state::Creator {
+                address: creator_key,
+                verified: false,
+                share: 100,
+            },
+            mpl_token_metadata::state::Creator {
+                address: ctx.accounts.mint_authority.key(),
+                verified: false,
+                share: 0,
+            },
+        ];
+        msg!("Creator Assigned");
+        let symbol = std::string::ToString::to_string("symb");
+        invoke(
+            &create_metadata_accounts_v2(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.payer.key(),
+                title,
+                symbol,
+                uri,
+                Some(creator),
+                1,
+                true,
+                false,
+                None,
+                None,
+            ),
+            account_info.as_slice(),
+        )?;
+        msg!("Metadata Account Created !!!");
+        let master_edition_infos = vec![
+            ctx.accounts.master_edition.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        msg!("Master Edition Account Infos Assigned");
+        invoke(
+            &create_master_edition_v3(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.master_edition.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.payer.key(),
+                Some(0),
+            ),
+            master_edition_infos.as_slice(),
+        )?;
+        msg!("Master Edition Nft Minted !!!");
+        Ok(())
+    }
     // #[access_control(ctx.accounts.validate())]
     // pub fn initialize_pool(ctx: Context<InitializePool>,bump:u8) -> ProgramResult{
     //     processor::init::initialize_pool(ctx, bump)
@@ -72,109 +177,5 @@ pub struct MintNFT<'info> {
 }
 
 
-pub fn mint_nft(
-    ctx: Context<MintNFT>,
-    creator_key: Pubkey,
-    uri: String,
-    title: String,
-) -> Result<()> {
-    msg!("Initializing Mint NFT");
-    let cpi_accounts = MintTo {
-        mint: ctx.accounts.mint.to_account_info(),
-        to: ctx.accounts.token_account.to_account_info(),
-        authority: ctx.accounts.payer.to_account_info(),
-    };
-msg!("CPI Accounts Assigned");
-    let cpi_program = ctx.accounts.token_program.to_account_info();
-    msg!("CPI Program Assigned");
-    let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-    msg!("CPI Context Assigned");
-    token::mint_to(cpi_ctx, 1)?;
-    msg!("Token Minted !!!");
-    let account_info = vec![
-        ctx.accounts.metadata.to_account_info(),
-        ctx.accounts.mint.to_account_info(),
-        ctx.accounts.mint_authority.to_account_info(),
-        ctx.accounts.payer.to_account_info(),
-        ctx.accounts.token_metadata_program.to_account_info(),
-        ctx.accounts.token_program.to_account_info(),
-        ctx.accounts.system_program.to_account_info(),
-        ctx.accounts.rent.to_account_info(),
-    ];
-    msg!("Account Info Assigned");
-    let creator = vec![
-        mpl_token_metadata::state::Creator {
-            address: creator_key,
-            verified: false,
-            share: 100,
-        },
-        mpl_token_metadata::state::Creator {
-            address: ctx.accounts.mint_authority.key(),
-            verified: false,
-            share: 0,
-        },
-    ];
-    msg!("Creator Assigned");
-    let symbol = std::string::ToString::to_string("symb");
-    invoke(
-        &create_metadata_accounts_v2(
-            ctx.accounts.token_metadata_program.key(),
-            ctx.accounts.metadata.key(),
-            ctx.accounts.mint.key(),
-            ctx.accounts.mint_authority.key(),
-            ctx.accounts.payer.key(),
-            ctx.accounts.payer.key(),
-            title,
-            symbol,
-            uri,
-            Some(creator),
-            1,
-            true,
-            false,
-            None,
-            None,
-        ),
-        account_info.as_slice(),
-    )?;
-    msg!("Metadata Account Created !!!");
-    let master_edition_infos = vec![
-        ctx.accounts.master_edition.to_account_info(),
-        ctx.accounts.mint.to_account_info(),
-        ctx.accounts.mint_authority.to_account_info(),
-        ctx.accounts.payer.to_account_info(),
-        ctx.accounts.metadata.to_account_info(),
-        ctx.accounts.token_metadata_program.to_account_info(),
-        ctx.accounts.token_program.to_account_info(),
-        ctx.accounts.system_program.to_account_info(),
-        ctx.accounts.rent.to_account_info(),
-    ];
-    msg!("Master Edition Account Infos Assigned");
-    invoke(
-        &create_master_edition_v3(
-            ctx.accounts.token_metadata_program.key(),
-            ctx.accounts.master_edition.key(),
-            ctx.accounts.mint.key(),
-            ctx.accounts.payer.key(),
-            ctx.accounts.mint_authority.key(),
-            ctx.accounts.metadata.key(),
-            ctx.accounts.payer.key(),
-            Some(0),
-        ),
-        master_edition_infos.as_slice(),
-    )?;
-    msg!("Master Edition Nft Minted !!!");
-    Ok(())
-}
 
-// let creator = vec![
-//             mpl_token_metadata::state::Creator {
-//                 address: creator_key,
-//                 verified: false,
-//                 share: 100,
-//             },
-//             mpl_token_metadata::state::Creator {
-//                 address: ctx.accounts.mint_authority.key(),
-//                 verified: false,
-//                 share: 0,
-//             },
-//         ];
+
--- contract/tests/contract.ts
+++ contract/tests/contract.ts
@@ -1,6 +1,6 @@
-import * as anchor from "@project-serum/anchor";
+// import * as anchor from "@project-serum/anchor";
 // import { Program } from "@project-serum/anchor";
-import { Contract } from "../target/types/contract";
+// import { Contract } from "../target/types/contract";
 
 // describe("contract", () => {
 //   // Configure the client to use the local cluster.
@@ -15,49 +15,3 @@ import { Contract } from "../target/types/contract";
 //   });
 // });
 
-import {
-  TOKEN_PROGRAM_ID,
-  createAssociatedTokenAccountInstruction,
-  getAssociatedTokenAddress,
-  createInitializeMintInstruction,
-  MINT_SIZE,
-} from "@solana/spl-token";
-import { LAMPORTS_PER_SOL } from "@solana/web3.js";
-const { PublicKey, SystemProgram } = anchor.web3; 
-const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
-      "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
-    );
-    const lamports: number =
-      await program.provider.connection.getMinimumBalanceForRentExemption(
-        MINT_SIZE
-      );
-    const getMetadata = async (
-      mint: anchor.web3.PublicKey
-    ): Promise<anchor.web3.PublicKey> => {
-      return (
-        await anchor.web3.PublicKey.findProgramAddress(
-          [
-            Buffer.from("metadata"),
-            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-            mint.toBuffer(),
-          ],
-          TOKEN_METADATA_PROGRAM_ID
-        )
-      )[0];
-    };
-    const getMasterEdition = async (
-      mint: anchor.web3.PublicKey
-    ): Promise<anchor.web3.PublicKey> => {
-      return (
-        await anchor.web3.PublicKey.findProgramAddress(
-          [
-            Buffer.from("metadata"),
-            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-            mint.toBuffer(),
-            Buffer.from("edition"),
-          ],
-          TOKEN_METADATA_PROGRAM_ID
-        )
-      )[0];
-    };
-    const mintKey: anchor.web3.Keypair = anchor.web3.Keypair.generate();
